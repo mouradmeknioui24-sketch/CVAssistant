27839fae831e08003dded497486d4fb2a44f2d63
unknown <mourad.meknioui@outlook.com>
2025-12-23 23:00:05 +0100

    first cv app

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from pypdf import PdfReader
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_chroma import Chroma
-import os
+from langchain_core.documents import Document
+from langchain_core.messages import SystemMessage, HumanMessage
 
+# -------------------- ENV & MODELS --------------------
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 llm = ChatOpenAI(model="gpt-4o", temperature=0)
 embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
