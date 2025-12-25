fb250921ba10c3ed72359c06cf0137ff6461de96
unknown <mourad.meknioui@outlook.com>
2025-12-25 12:06:27 +0100

    add

--- app.py
+++ app.py
@@ -59,6 +59,15 @@ st.markdown("""
         conversations before the first interview.
     </div>
 </div>
+<div style="display:none;">
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "uqq6o9ppuj");
+    </script>
+</div>
 """, unsafe_allow_html=True)
 
 # ==================================================
