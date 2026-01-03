68e61dbaa63a15a9947cc92fcbe17bc9eb0e00b5
unknown <mourad.meknioui@outlook.com>
2026-01-03 13:01:21 +0100

    h

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ import cloudinary.uploader
 import streamlit as st
 
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    cloud_name=os.getenv("cloud_name"),
+    api_key=os.getenv("api_key"),
+    api_secret=os.getenv("api_secret"),
     secure=True
 )
 
